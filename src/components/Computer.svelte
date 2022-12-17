7fba02f0b1f792bdea8727890db2f93ebdf5d773
Patato <pathonbusiness@gmail.com>
2022-12-17 16:53:49 +0700

    loading and age counter

--- src/components/Computer.svelte
+++ src/components/Computer.svelte
@@ -15,7 +15,12 @@
 
 <div>
 	<div class="mt-7 ml-[5.7rem] w-[11.8rem] h-[8.8rem] absolute bg-black">
-		<img class="object-contain w-full h-full" src={link} alt="" />
+		{#if link}
+			<img class="object-contain w-full h-full" src={link} alt="" />
+		{:else}
+			<!-- else content here -->
+			<h1 class="text-green-800">Loading...</h1>
+		{/if}
 	</div>
 	<img class="" src="/computer.png" alt="Computer" />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
 	import Computer from '../components/Computer.svelte';
+	let age: string = ' ';
+	setInterval(() => {
+		let time =
+			(new Date().valueOf() - new Date(1206837420000).valueOf()) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
+		age = time.toString().substring(0, 12);
+	}, 25);
 </script>
 
 <main class="w-full h-screen flex flex-col items-center justify-center bg-slate-800">
@@ -14,7 +20,7 @@
 					<hr />
 					<h2 class="content font-bold text-xl">I'm ...</h2>
 					<ul class="content text-xl">
-						<li>- 14.123456 years old</li>
+						<li>- {age} years old</li>
 						<li>- from Thailand</li>
 						<li>- a full-stack developer</li>
 						<li>- a 3D modeler?</li>
