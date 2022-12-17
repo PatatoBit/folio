64a267ff3d4b190a75e95db39fef5e7514ccbe6c
Patato <pathonbusiness@gmail.com>
2022-12-17 17:00:09 +0700

    add head

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,27 @@
 	}, 25);
 </script>
 
+<svelte:head>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<title>patato.live</title>
+	<meta name="author" content="Patato" />
+	<meta name="author" content="patato" />
+	<meta name="description" content="I swear this is not a porn site" />
+
+	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+	<link rel="shortcut icon" href="/noporn.png" type="image/x-icon" />
+	<meta name="twitter:title" content="Patato" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@patatotota" />
+	<meta name="twitter:creator" content="@patatotota" />
+
+	<meta property="og:site_name" content="patato.live" />
+	<meta name="og:title" content="Patato" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="/bing.png" />
+</svelte:head>
+
 <main class="w-full h-screen flex flex-col items-center justify-center bg-slate-800">
 	<div class="max-w-5xl flex md:flex-row flex-col md:gap-16 gap-8">
 		<div>
